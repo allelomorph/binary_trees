c63711a974a2e7688b68056f0dfa60ed2a34a8e9
Samuel Pomeroy <1690@holbertonschool.com>
2020-07-25 06:49:49 +0000

    first attempt t24

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,49 @@
+/* 24. Is BST */
+#include "binary_trees.h"
+#include <limits.h>
+
+/**
+ * check_if_BST - recursive helper to binary_tree_is_bst
+ * @tree: tree to check for BST
+ * @prev: pointer to int, passed "by reference" to update during recursion
+ * Return: 1 if valid if `tree` is valid BST, or 0 if not or `tree` is NULL
+ */
+int check_if_BST(const binary_tree_t *tree, int *prev)
+{
+	if (tree)
+	{
+		/* in-order traversal */
+		if (!check_if_BST(tree->left, prev))
+			return (0);
+
+		/* checks both for repeat values and for left_child > parent */
+		/* and right_child < parent */
+		if (tree->n <= *prev)
+			return (0);
+
+		/* prev updates to current */
+		*prev = tree->n;
+
+		return check_if_BST(tree->right, prev);
+	}
+	/* recursion has reached an edge of the tree */
+	return (1);
+}
+
+/**
+ * binary_tree_is_bst - tests if binary tree is a binary search tree: left
+ * subtree of each node only has values less than node, right subtree of each
+ * node only has values greater than node, tree is a BST at each node, and no
+ * values appear twice
+ * @tree: tree to check for BST
+ * Return: 1 if valid if `tree` is valid BST, or 0 if not or `tree` is NULL
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	int prev = INT_MIN;
+
+	if (!tree)
+		return (0);
+
+	return check_if_BST(tree, &prev);
+}

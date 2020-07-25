eed0ca7139906728a5f6d81777c84d9d399acb10
Samuel Pomeroy <1690@holbertonschool.com>
2020-07-25 07:25:34 +0000

    updated comments

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -6,18 +6,23 @@
  * check_if_BST - recursive helper to binary_tree_is_bst
  * @tree: tree to check for BST
  * @prev: pointer to int, passed "by reference" to update during recursion
- * Return: 1 if valid if `tree` is valid BST, or 0 if not or `tree` is NULL
+ * Return: 1 valid if `tree` is valid BST, or 0 if not or `tree` is NULL
  */
 int check_if_BST(const binary_tree_t *tree, int *prev)
 {
 	if (tree)
 	{
-		/* in-order traversal */
+		pritnf("tree->n: %i prev: %i\n", tree->n, *prev);
+		/* in-order traversal: recurse left, test, recurse right */
+		/* branching left tests against prev inherited from parent */
+		/* while branching right tests against prev = parent->n */
 		if (!check_if_BST(tree->left, prev))
 			return (0);
 
-		/* checks both for repeat values and for left_child > parent */
-		/* and right_child < parent */
+		/**
+		 * checks both for repeat values and for left_child < parent
+		 * and right_child > parent
+		 */
 		if (tree->n <= *prev)
 			return (0);
 
@@ -31,12 +36,12 @@ int check_if_BST(const binary_tree_t *tree, int *prev)
 }
 
 /**
- * binary_tree_is_bst - tests if binary tree is a binary search tree: left
- * subtree of each node only has values less than node, right subtree of each
- * node only has values greater than node, tree is a BST at each node, and no
- * values appear twice
+ * binary_tree_is_bst - uses recursive helper to test is binary tree is binary
+ * search tree: left subtree of each node only has values less than node,
+ * right subtree of each node only has values greater than node, tree is a
+ * BST at each node, and no values appear twice
  * @tree: tree to check for BST
- * Return: 1 if valid if `tree` is valid BST, or 0 if not or `tree` is NULL
+ * Return: 1 valid if `tree` is valid BST, or 0 if not or `tree` is NULL
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {

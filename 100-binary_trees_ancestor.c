4a0af889aa5f0cfcc688ffba1f8106332fcd03ff
Samuel Pomeroy <1690@holbertonschool.com>
2020-12-21 07:15:53 +0000

    t19 NULL input checks and if arg nodes are in same tree

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -53,9 +53,21 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
 	binary_tree_t *root = (binary_tree_t *)first;
+	binary_tree_t *root2 = (binary_tree_t *)second;
+
+	/* bad input */
+	if (!first || !second)
+		return (NULL);
 
 	while (root->parent)
 		root = root->parent;
+
+	/* safety check if both are in the same tree, may be more elegant */
+	/* solution to be made by modifying bt_find_lca() */
+	while (root2->parent)
+		root2 = root2->parent;
+	if (root != root2)
+		return (NULL);
 
 	return (bt_find_lca(root, first, second));
 }

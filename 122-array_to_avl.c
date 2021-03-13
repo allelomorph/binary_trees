7321889797241ae5a173084583ab663f4022473c
Samuel Pomeroy <1690@holbertonschool.com>
2021-03-13 10:06:56 +0000

    prep for t30,31,32

---
+++ 122-array_to_avl.c
@@ -0,0 +1,35 @@
+/* 32. AVL - Array to AVL */
+#include "binary_trees.h"
+
+
+/**
+ * array_to_bst - builds a Binary Search Tree from an array
+ *
+ * @array: array of integers, may be unsorted and have repeating values
+ * @size: amount of array members
+ * Return: pointer to head of new BST constructed from array
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *tree = NULL;
+	size_t i, j;
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		/* check if NULL return is for repeat value */
+		if (bst_insert(&tree, array[i]) == NULL)
+		{
+			for (j = 0; j < size && array[j] != array[i]; j++)
+			{}
+
+			/* not a repeating value, bst_insert failure */
+			if (j == i)
+				return (NULL);
+		}
+	}
+
+	return (tree);
+}
